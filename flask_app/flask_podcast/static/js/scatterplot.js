874e1194f77042f10792f91ee735b3a3b9fcd87d
Lindsay Vass <lindsay.k.morgan@gmail.com>
2016-02-02 15:02:01 -0800

    flask app

--- flask_app/flask_podcast/static/js/scatterplot.js
+++ flask_app/flask_podcast/static/js/scatterplot.js
@@ -16,11 +16,6 @@ var w = 400;
 var h = 400;
 var padding = 30;
 
-var svg = d3.select("#scatterplot")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
-
 var xScale = d3.scale.linear()
 					 .domain([0, d3.max(dataset, function (d) { return d[0]; })])
 					 .range([padding, w - padding * 2]);
@@ -29,24 +24,32 @@ var yScale = d3.scale.linear()
 					 .domain([0, d3.max(dataset, function (d) { return d[1]; })])
 					 .range([h - padding, padding]);
 
+<<<<<<< Updated upstream
 var rScale = d3.scale.linear()
 					 .domain([0, d3.max(dataset, function (d) { return d[1]; })])
 					 .range([2, 5]);
+=======
+var tip = d3.tip()
+	.attr('class', 'd3-tip')
+	.offset([-10, 0])
+	.html(function(d) {
+		return d.name;
+	});
 
-// var xAxis = d3.svg.axis()
-// 				  .scale(xScale)
-// 				  .orient("bottom")
-// 				  .ticks(5);
+var svg = d3.select("#scatterplot")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
+>>>>>>> Stashed changes
 
-// var yAxis = d3.svg.axis()
-// 				  .scale(yScale)
-// 				  .orient("left")
-// 				  .ticks(5);
+svg.call(tip);
+
 
 var circles = svg.selectAll("circle")
 	.data(dataset)
 	.enter()
 	.append("circle")
+<<<<<<< Updated upstream
 	.attr("cx", function (d) { return xScale(d[0]); })
 	.attr("cy", function (d) { return yScale(d[1]); })
 	.attr("r",  function (d) { return rScale(d[1]); })
@@ -54,6 +57,71 @@ var circles = svg.selectAll("circle")
 		
 		var xPosition = parseFloat(d3.select(this).attr("cx")) - 14;
 		var yPosition = parseFloat(d3.select(this).attr("cy")) - 14;
+=======
+	.attr("cx", function (d) { return xScale(d.x_coord); })
+	.attr("cy", function (d) { return yScale(d.y_coord); })
+	.attr("r",  radius)
+	.style("fill", function (d) {
+			var returnColor;
+			if (d.searched === true) {
+				returnColor = "#8C3FC6";
+			} else {
+				returnColor = "black";
+			}
+			return returnColor;
+		})
+	.on("mouseover", tip.show)
+	.on("mouseout", tip.hide);
+
+
+
+	// .on("mouseover", function (d) {
+	// 	//get position
+	// 	var xPosition = parseFloat(d3.select(this).attr("cx"));
+	// 	var yPosition = parseFloat(d3.select(this).attr("cy"));
+
+	// 	//create tooltip
+	// 	svg.append("text")
+	// 		.attr("id", "tooltip")
+	// 		.attr("x", xPosition)
+	// 		.attr("y", yPosition)
+	// 		.attr("text-anchor", "middle")
+	// 		.attr("font-family", "sans-serif")
+	// 		.attr("font-size", "11px")
+	// 		.attr("font-weight", "bold")
+	// 		.attr("fill", "black")
+	// 		.text()
+	// })
+
+
+
+
+
+		// .append("div tooltip")
+		// .on("mouseover", function (d) { return tooltip.style("visibility")})
+	// .on("mouseover", function (d) { 
+		
+	// 	var xPosition = parseFloat(d3.select(this).attr("cx"));
+	// 	var yPosition = parseFloat(d3.select(this).attr("cy"));
+
+	// 	// create the tooltip label
+	// 	svg.append("title")
+	// 		.attr("id", "tooltip")
+	// 		.attr("x", xPosition)
+	// 		.attr("y", yPosition)
+	// 		.attr("text-anchor", "middle")
+	// 		.attr("font-family", "sans-serif")
+	// 		.attr("font-size", "11px")
+	// 		.attr("font-weight", "bold")
+	// 		.attr("fill", "black")
+	// 		.text(function(d) { return d.name; })
+
+	// })
+	// .on("mouseout", function() {
+	// 	//remove the tooltip
+	// 	d3.select("#tooltip").remove();
+	// });
+>>>>>>> Stashed changes
 
 		d3.select("#tooltip")
 			.style("left", xPosition + "px")
