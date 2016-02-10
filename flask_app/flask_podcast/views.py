271e023170a038fd3e494f400d625f48d4a1b6c8
Lindsay Vass <lindsay.k.morgan@gmail.com>
2016-02-09 16:22:00 -0800

    use simserver for keyword output

--- flask_app/flask_podcast/views.py
+++ flask_app/flask_podcast/views.py
@@ -284,3 +284,63 @@ def podcast_output_simserver():
 
   # return render_template("output.html", podcast_results_no_self=podcast_results_no_self, podcast_results=merge_dict, search_name=podcast_name)
   return render_template("output_force_layout.html", podcast_results_no_self=podcast_results_no_self, podcast_results=merge_dict, search_name=podcast_name)
+
+@app.route('/keyword_output_simserver', methods=['GET'])
+def keyword_output_simserver():
+  num_results=100
+
+  search = request.args.get('keyword')
+  doc = {'tokens' : pp.preprocess_text(search)} 
+  
+  sim_tuple = server.find_similar(doc)
+
+  sim_df = pd.DataFrame(sim_tuple, columns=['id', 'similarity', 'null'])
+
+  del(sim_df['null'])
+
+  # take top ids
+  ids = list(sim_df['id'].values)
+  ids = [str(int(x)) for x in ids]
+  ids = ids[:num_results]
+  
+  # query the db
+  query = """
+  SELECT name, view_url, artwork_url100, id, raw_summary
+  FROM podcast
+  WHERE id IN %s;
+  """
+  query = query.replace('\n', ' ')
+
+  cursor.execute(query % (pg_int_array(ids)))
+  query_results = cursor.fetchall()
+
+  # convert to dataframe
+  columnNames = ['name', 'view_url', 'artwork_url100', 'id', 'summary']
+  podcast_results = pd.DataFrame(columns=columnNames)
+  podcast_results['name'] = [x[0] for x in query_results]
+  podcast_results['view_url'] = [x[1] for x in query_results]
+  podcast_results['artwork_url100'] = [x[2] for x in query_results]
+  podcast_results['id'] = [x[3] for x in query_results]
+  podcast_results['summary'] = [x[4] for x in query_results]
+
+  # merge with pca data
+  sim_df['id'] = [int(x) for x in sim_df['id']]
+  table_results = pd.merge(sim_df, podcast_results, how = 'inner', left_on='id', right_on='id')
+  scatter_results = table_results.copy(deep=True)
+
+  # convert to jsonlike
+  podcast_results = []
+  for i in range(num_results):
+    podcast_results.append(dict(similarity=table_results.iloc[i]['similarity'], name=table_results.iloc[i]['name'].decode('utf-8'), view_url=table_results.iloc[i]['view_url'], artwork_url100=table_results.iloc[i]['artwork_url100'], id=table_results.iloc[i]['id']))
+
+  podcast_results_no_self = podcast_results[1:]
+
+  print podcast_results_no_self
+
+  scatter_results['name'] = [str(x).replace('"', "'") for x in scatter_results['name']]
+  scatter_results['summary'] = [str(x).replace('"', "'") for x in scatter_results['summary']]
+  scatter_results['summary'] = [x.decode('utf-8') for x in scatter_results['summary']]
+  
+  merge_dict = pd_to_json_dict(scatter_results)
+
+  return render_template("output_force_layout.html", podcast_results_no_self=podcast_results, podcast_results=merge_dict, search_name=search)
