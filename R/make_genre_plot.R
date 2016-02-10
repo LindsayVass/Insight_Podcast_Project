be43bdb0f8fbed3b8776cc5e3c9d937a78cbb33b
Lindsay Vass <lindsay.k.morgan@gmail.com>
2016-02-09 20:21:52 -0800

    plot as percentage

--- R/make_genre_plot.R
+++ R/make_genre_plot.R
@@ -39,20 +39,20 @@ pGenre <- ggplot(genreSummary, aes(x = genre_name, y = Count)) +
   xlab('Number of Podcasts')
 ggsave('r_plots/genre_counts.pdf', width = 20, height = 20)
 
-# top 20 genres
-pTop20 <- genreSummary %>%
-  arrange(desc(Count)) %>%
-  top_n(20) %>%
-  ggplot(aes(x = genre_name, y = Count)) +
+# top 10 genres
+pTop10 <- genreSummary %>%
+  mutate(Proportion = Count / length(unique(podcast_df$id))) %>%
+  arrange(desc(Proportion)) %>%
+  top_n(10) %>%
+  ggplot(aes(x = genre_name, y = Proportion)) +
   geom_bar(stat='identity') +
-  geom_text(aes(x = genre_name, y = 10, label = genre_name, hjust = 0), colour = "white", size = 4.5) +
-  coord_flip(ylim=c(50,1250)) +
+  geom_text(aes(x = genre_name, y = 0.01, label = genre_name, hjust = 0), colour = "white", size = 4.5) +
+  scale_y_continuous(labels = scales::percent) +
+  coord_flip() +
   theme_fivethirtyeight() +
   theme(axis.text.y = element_blank(),
         axis.ticks.y = element_blank(),
         axis.text.x = element_text(size=18),
         axis.title.x = element_text(size=18),
-        axis.title.y = element_text(size=18)) +
-  ylab('Podcasts') +
-  xlab('Top 20 Genres')
-ggsave('r_plots/genre_counts_top20.pdf', width = 13, height = 10)
+        axis.title.y = element_text(size=18)) 
+ggsave('r_plots/genre_counts_top10.pdf', width = 13, height = 10)
